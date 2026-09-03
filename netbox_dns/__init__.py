e5b218778372a0df5e67d5412386cd4ca2357307
Peter Eckel <pe-git@hindenburgring.com>
2026-09-03 16:11:57 +0200

    Prepared release 1.7.1

--- netbox_dns/__init__.py
+++ netbox_dns/__init__.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 from netbox.plugins import PluginConfig
 from netbox.plugins.utils import get_plugin_config
 
-__version__ = "1.7.0"
+__version__ = "1.7.1"
 
 
 _branching_hooks_registered = False
