a755bfc3d42ad4aa344511882b97bf0fe45c69fb
Peter Eckel <pe-git@hindenburgring.com>
2026-09-03 16:03:46 +0200

    Prepared release 1.5.14

--- netbox_dns/tests/test_netbox_dns.py
+++ netbox_dns/tests/test_netbox_dns.py
@@ -7,7 +7,7 @@ from utilities.testing.api import APITestCase
 
 class NetBoxDNSVersionTestCase(SimpleTestCase):
     def test_version(self):
-        assert __version__ == "1.5.12"
+        assert __version__ == "1.5.14"
 
 
 class AppTest(APITestCase):
